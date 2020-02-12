e26b0e5ee66713c011b7a9f3e3d09e9d78290427
David Pinhas <45331381+davidpinhas@users.noreply.github.com>
2020-02-12 23:36:10 +0200

    Update GuessTheNumber.py

--- python-games/GuessTheNumber.py
+++ python-games/GuessTheNumber.py
@@ -4,26 +4,26 @@ print("\nWelcome to Guess The Number idiot")
 print("You only got 5 tries, let see how slow you are\n")
 
 guessesTaken = 0
-number = random.randint(1, 100)
+number = random.randint(1, 3)
 
 while guessesTaken < 5:
     print("So...What's your guess? ")
-    guess = input()
-    guess = int(guess)
+    guess = int(input())
     guessesTaken = guessesTaken + 1
 
     if guess < number:
         print("That's wayyy too low you fucking moron\n")
-    if guess > number:
-        print("That's too high, loser\n")
-    if guess == number:
+    elif guess > number:
+        print("That's too high loser\n")
+    elif guess == number:
         break
 
-if (guess == number) and guessesTaken < 5:
+if (guess == number) and guessesTaken < 4:
     guessesTaken = str(guessesTaken)
     print("Wowowow! You are a fucking cheater! only " + guessesTaken + " tries?!")
-if guess == number:
     print("Noice!")
-if guess != number:
+elif guess == number:
+    print("Noice!")
+elif guess != number:
     guessesTaken = str(guessesTaken)
     print("Such a loser... It took you " + guessesTaken + " tries to show us the genius you are not.\n")
